ada4f982a4624ad6c98debb860aaaab0714ec93c
Jordi Mairena <jordim95@hotmail.com>
2017-11-20 09:53:59 -0600

    terminado

--- src/components/empleador.vue
+++ src/components/empleador.vue
@@ -7,7 +7,9 @@
       <input v-model="empleador.ceo" type="text"  placeholder="CEO ">
       <input v-model="empleador.direccion" type="text" placeholder="Direccion ">
       <input v-model="empleador.telefono" type="text" onkeypress='return event.charCode >= 48 && event.charCode <= 57' minlength="8" maxlength="8" placeholder="Numero de Telefono">
+      <input v-model="empleador.password" type="password" placeholder="Password">
       <a name="empleador.aceptar" class="waves-effect waves-light btn" v-on:click="CreoEmpresa()">  Aceptar</a>
+
 
     </div>
   </div>
@@ -23,85 +25,96 @@ export default {
         rtn: "",
         ceo: "",
         direccion: "",
-        telefono: ""
+        telefono: "",
+        password: "",
+        scope: ""
       },
     }
   },
-    methods:{
-      //no se cierra bien
-      CreoEmpresa:function(){
-        const {nombreEmpresa,rtn,ceo,Direccion,Telefono} = this.empleador;
-        // alert("Nombre Empresa: " + nombreEmpresa + "RTN: "+rtn);
-        if(this.empleador.nombre_empresa.length===0 &&this.empleador.ceo.length===0 &&this.empleador.direccion.length===0){
-          Materialize.toast('Un campo esta vacio!', 3000)
-        }else if (this.empleador.rtn.length != 13) {
-          // alert("length must be exactly 13 characters")
-          Materialize.toast('RTN debe contener 13 numeros', 3000)
-        }else if(this.empleador.telefono.length !=8){
-          Materialize.toast('Numero de telefono invalido!', 3000)
-        }else{
-          //   sweetAlert(
-          //     'Good job!',
-          //     'You clicked the button!',
-          //     'success'
-          //   )
-        }
-          this.$http.post(`http://localhost:8000/empleadores/create`,this.empleador).then((response)=>{
-            if(response.body.success==true){
-              sweetAlert({
-                title: "Genial!!!",
-                text: "Empleador ha sido creado con exito!",
-                icon: "success",
-              });
-              this.empleador.nombre_empresa="";
-              this.empleador.rtn="";
-              this.empleador.ceo="";
-              this.empleador.direccion="";
-              this.empleador.telefono="";
-            }else {
-              alert("ijihiu");
-            }
-          });
+  methods:{
+    //no se cierra bien
+    CreoEmpresa:function(){
+      const {nombreEmpresa,rtn,ceo,Direccion,Telefono} = this.empleador;
+      // alert("Nombre Empresa: " + nombreEmpresa + "RTN: "+rtn);
+      if(this.empleador.nombre_empresa.length===0 &&this.empleador.ceo.length===0 &&this.empleador.direccion.length===0){
+        Materialize.toast('Un campo esta vacio!', 3000)
+      }else if (this.empleador.rtn.length != 13) {
+        // alert("length must be exactly 13 characters")
+        Materialize.toast('RTN debe contener 13 numeros', 3000)
+      }else if(this.empleador.telefono.length !=8){
+        Materialize.toast('Numero de telefono invalido!', 3000)
+      }else{
+        this.$http.post(`http://localhost:8000/empleadores/create`,this.empleador).then((response)=>{
+          if(response.body.success==true){
+            sweetAlert({
+              title: "Genial!!!",
+              text: "Empleador ha sido creado con exito!",
+              icon: "success",
+            });
+            this.empleador.nombre_empresa="";
+            this.empleador.rtn="";
+            this.empleador.ceo="";
+            this.empleador.direccion="";
+            this.empleador.telefono="";
+            this.empleador.password="";
+            this.empleador.scope="";
+          }else {
+            sweetAlert(
+              'Oops...',
+              'Error!',
+              'error'
+            )
+          }
+        });
+      }
 
-      },
 
     },
+
+  },
+  mounted(){
+    const {nombre, identidad, password, scope} = this.$route.params;
+    this.empleador.nombre_empresa = nombre;
+    this.empleador.rtn = identidad;
+    this.empleador.password = password;
+    this.empleador.scope = scope;
   }
-</script>
+  }
+  </script>
 
-<style lang="css">
+  <style lang="css">
 
-.contenido{
-  position: relative;
-  z-index: 1;
-  background-color: white;
-  max-width: 70%;
-  max-height: 0%;
-  margin: 0 auto 0;
-  padding: 45px;
-  text-align: top center;
-  box-shadow: 10px 10px 5px #888888;
-}
-
-
-.lol{
-  margin: 0;
-}
-body{
-  background: #76b852;
-}
-@media(max-width: 480px){
   .contenido{
     position: relative;
     z-index: 1;
     background-color: white;
-    max-width: 380px;
+    max-width: 70%;
     max-height: 0%;
-    margin: 0 auto 100px;
+    margin: 0 auto 0;
     padding: 45px;
-    text-align: center;
+    text-align: top center;
     box-shadow: 10px 10px 5px #888888;
   }
-}
 
-</style>
+
+  .lol{
+    margin: 0;
+  }
+  body{
+    background: #76b852;
+  }
+  @media(max-width: 480px){
+    .contenido{
+      position: relative;
+      z-index: 1;
+      background-color: white;
+      max-width: 380px;
+      max-height: 0%;
+      margin: 0 auto 100px;
+      padding: 45px;
+      text-align: center;
+      box-shadow: 10px 10px 5px #888888;
+    }
+  }
+
+  </style>
