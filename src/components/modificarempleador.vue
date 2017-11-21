ca6da601aaed3fcf071207185fe7212f443fbf92
guillelopez22 <guillelopez_13@hotmail.com>
2017-11-20 22:20:05 -0600

    re-routing

--- src/components/modificarempleador.vue
+++ src/components/modificarempleador.vue
@@ -9,6 +9,7 @@
       <input v-model="empleador.telefono" type="text" onkeypress='return event.charCode >= 48 && event.charCode <= 57' minlength="8" maxlength="8">
       <input v-model="empleador.password" type="password" >
       <a name="empleador.aceptar" class="waves-effect waves-light btn" v-on:click="editEmpleador()">  Aceptar</a>
+      <a name="empleador.cancelar" class="waves-effect waves-light btn" v-on:click="cancelar()">Cancelar</a>
     </div>
   </div>
 </template>
@@ -25,7 +26,7 @@ export default {
   },
   methods:{
     getEmpleador(){
-      localStorage.setItem("identidad","4444444444444");
+      localStorage.setItem("identidad","1111111111111");
       var rtn = localStorage.getItem('identidad');
       this.$http.get('http://localhost:8000/empleadores/searchbyRTN/'+rtn).then((response)=>{
         this.empleador=response.body[0];
@@ -36,59 +37,64 @@ export default {
       var _id= this.empleador._id;
       this.$http.put('http://localhost:8000/empleadores/update/'+_id,this.empleador).then((response)=>{
         console.log(response.body);
-        if (response.body.success==true){           
+        if (response.body.success==true){
           sweetAlert({
             title:"Correcto",
             text:"Modificado con exito",
             icon:"sucess"
           });
+          localStorage.setItem("identidad", this.empleador.rtn);
+          this.$router.push({path:"/home"});
         }else{
-           sweetAlert(
-             'error',
-             'Error!',
-             'error'
-           )
-         }
+          sweetAlert(
+            'error',
+            'Error!',
+            'error'
+          )
+        }
       });
+    },
+    cancelar(){
+      this.$router.push({path:"/home"});
     }
   },
-    beforeMount(){
-      this.getEmpleador();
-    }
+  beforeMount(){
+    this.getEmpleador();
+  }
 }
 
 </script>
 
-  <style lang="css">
+<style lang="css">
+.contenido{
+  position: relative;
+  z-index: 1;
+  background-color: white;
+  max-width: 70%;
+  max-height: 0%;
+  margin: 0 auto 0;
+  padding: 45px;
+  text-align: top center;
+  box-shadow: 10px 10px 5px #888888;
+}
+.form-empleador{
+  margin: 0;
+}
+body{
+  background: #76b852;
+}
+@media(max-width: 480px){
   .contenido{
     position: relative;
     z-index: 1;
     background-color: white;
-    max-width: 70%;
+    max-width: 380px;
     max-height: 0%;
-    margin: 0 auto 0;
+    margin: 0 auto 100px;
     padding: 45px;
-    text-align: top center;
+    text-align: center;
     box-shadow: 10px 10px 5px #888888;
   }
-  .form-empleador{
-    margin: 0;
-  }
-  body{
-    background: #76b852;
-  }
-  @media(max-width: 480px){
-    .contenido{
-      position: relative;
-      z-index: 1;
-      background-color: white;
-      max-width: 380px;
-      max-height: 0%;
-      margin: 0 auto 100px;
-      padding: 45px;
-      text-align: center;
-      box-shadow: 10px 10px 5px #888888;
-    }
-  }
+}
 
-  </style>
+</style>
